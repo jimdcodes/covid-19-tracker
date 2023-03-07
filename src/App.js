5a9da8af2a5ea1655882acd64468805e91f4773c
jimdcodes <jdinhcodes@gmail.com>
2023-03-06 19:40:46 -0800

    Adding markers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div className="app">
       <h1>Lets build a COVID 19 tracker!</h1>
+
+      {/* Header */}
+      {/* Title + Select input dropdown field */}
+
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+      
     </div>
   );
 }
