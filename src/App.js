cf74f1ff20180515c3eab7a9f439ee9567306166
jimdcodes <jdinhcodes@gmail.com>
2023-03-06 20:11:05 -0800

    Adding countries to dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import { FormControl, MenuItem, Select } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([
+    'USA','UK','INDIA'
+  ]);
+
+  // STATE = How to write a variable in REACT
+
   return (
-    <div className="app">
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl className="app__dropdown">
-        <Select
-        variant="outlined"
-        value="abc"
-        >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option two</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-        </Select>
-      </FormControl>
+    <div className="app">      
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        {/* Adding dropdown */}
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and show a drop down list of the options */}
+
+            {
+              countries.map(country => (
+                <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+
+            {/* Dropdown choices */}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option two</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>      
 
       {/* Header */}
       {/* Title + Select input dropdown field */}
