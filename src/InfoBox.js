9139cd720473b20a89085e6581245b928c0653e3
jimdcodes <jdinhcodes@gmail.com>
2023-03-14 19:26:04 -0700

    Adding commas on every thousanths place

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,17 +1,18 @@
 // rfce : React functional component export
-import { Card, CardContent, Typography } from '@mui/material'
-import React from 'react'
+import { Card, CardContent, Typography } from '@mui/material';
+import React from 'react';
+import numeral from 'numeral';
 
 function InfoBox({ title, cases, total }) {
   return (
     <Card className="infoBox">
         <CardContent>
             <Typography className="infoBox__title" color="textSecondary">
-                {title}
+                <strong>{title}</strong>
                 </Typography>            
-            <h2 className="infoBox__cases">{cases}</h2>
+            <h2 className="infoBox__cases">{numeral(cases).format(",0")}</h2>
             <Typography className="infoBox__total" color="textSecondary">
-                {total} Total
+                {numeral(total).format(",0")} Total
             </Typography>
         </CardContent>
     </Card>
