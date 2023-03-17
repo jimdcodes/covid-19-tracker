3c64d341e3f11c304957cfb5d20bfedb2736e47c
jimdcodes <jdinhcodes@gmail.com>
2023-03-16 20:47:51 -0700

    Save Mark for working graph.

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -40,7 +40,7 @@ function LineGraph() {
   // https://disease.sh/v3/covid-19/historical/all?lastdays=30
 
   useEffect(() => {
-    fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=30')
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=30")
     .then(response => response.json())
     .then(data => {
       //console.log(data);
